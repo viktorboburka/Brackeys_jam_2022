d4b36cdb40f8367825eb1c9a60193bfda9ea7f6a
viktorboburka <viktor@boburka.cz>
2022-02-20 17:49:47 +0100

    terminal velocity + grav scale

--- GravityPuzzler/Assets/GravityControl.cs
+++ GravityPuzzler/Assets/GravityControl.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class GravityControl : MonoBehaviour
 {
+    public float terminalVelocity = 10.0f;
+    public float gravityScale = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.gravity *= 2;
+        Physics2D.gravity *= gravityScale;
+        GameObject[] allRigidBodyObjects = GameObject.FindObjectsOfType(typeof (GameObject)) as GameObject[];
+        foreach( GameObject go in allRigidBodyObjects) {
+            Debug.Log(go.ToString());
+            Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
+            if (rb == null) continue;
+            rb.drag = GetDragFromAcceleration(Physics.gravity.magnitude, terminalVelocity);
+        }
     }
 
     // Update is called once per frame
@@ -17,5 +27,13 @@ public class GravityControl : MonoBehaviour
             Debug.Log("switch gravity");
             Physics2D.gravity = -Physics2D.gravity;
         }
+    }
+
+
+    public static float GetDrag(float aVelocityChange, float aFinalVelocity) {
+        return aVelocityChange / ((aFinalVelocity + aVelocityChange) * Time.fixedDeltaTime);
+    }
+    public static float GetDragFromAcceleration(float aAcceleration, float aFinalVelocity) {
+        return GetDrag(aAcceleration * Time.fixedDeltaTime, aFinalVelocity);
     }
 }
