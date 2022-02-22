9d8844179c9c18b6647ac2a2859dbbc7a587af22
Hans6565 <93428415+Hans6565@users.noreply.github.com>
2022-02-22 14:24:40 +0100

    KillerPlatform

--- GravityPuzzler/Assets/Scenes/Scripts/Player.cs
+++ GravityPuzzler/Assets/Scenes/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : GravityInfluenced
     private float _acceleration = 130;
     private float _accelerationAir = 20;
     private string _gravityDir;
+    private bool _isDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,36 +25,60 @@ public class Player : GravityInfluenced
     void calculateMovement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
+        if (!_isDead)
+        {
 
-        var hit = Physics2D.Raycast(transform.position, Physics2D.gravity, 1.02f, ~(1 << 2));
-        Vector2 velVec;
-        switch(_gravityDir) {
-            case "down":
-                velVec = new Vector2(horizontalInput, 0);
-                break;
-            case "up":
-                velVec = new Vector2(-horizontalInput, 0);
-                break;
-            case "left":
-                velVec = new Vector2(0, -horizontalInput);
-                break;
-            case "right":
-                velVec = new Vector2(0, horizontalInput);
-                break;
-            default:
+            var hit = Physics2D.Raycast(transform.position, Physics2D.gravity, 1.02f, ~(1 << 2));
+            Vector2 velVec;
+            switch (_gravityDir)
+            {
+                case "down":
+                    velVec = new Vector2(horizontalInput, 0);
+                    break;
+                case "up":
+                    velVec = new Vector2(-horizontalInput, 0);
+                    break;
+                case "left":
+                    velVec = new Vector2(0, -horizontalInput);
+                    break;
+                case "right":
+                    velVec = new Vector2(0, horizontalInput);
+                    break;
+                default:
 
-                velVec = new Vector2(horizontalInput, 0);
-                break;
+                    velVec = new Vector2(horizontalInput, 0);
+                    break;
+            }
+            if (hit)
+            {
+                GetComponent<Rigidbody2D>().velocity += velVec * _acceleration * Time.deltaTime;
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity += velVec * _accelerationAir * Time.deltaTime;
+            }
+        } else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         }
-        if (hit) {
-            GetComponent<Rigidbody2D>().velocity += velVec * _acceleration * Time.deltaTime;
-        }
-        else {
-            GetComponent<Rigidbody2D>().velocity += velVec * _accelerationAir * Time.deltaTime;
-        }
+        
     }
 
     public void rotateControls(string gravity) {
         _gravityDir = gravity;
     }
+   
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "KillerPlatform")
+        {
+            _isDead = true;
+            Debug.Log("you are dead");
+        }
+    }
+    public bool isDead()
+    {
+        return _isDead;
+    }
 }
+
