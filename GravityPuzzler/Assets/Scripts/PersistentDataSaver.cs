c3d5c49c34f1133d3f7460156aa4006a30b417c5
Isabella Skořepová <isabella@skorepova.info>
2022-02-23 12:52:12 +0100

    add PersistentData class

---
+++ GravityPuzzler/Assets/Scripts/PersistentDataSaver.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public sealed class PersistentDataSaver {
+    static PersistentData _instance;
+    public static string DataDirectory {
+        get {
+            if (Application.platform == RuntimePlatform.Android) return Path.Combine(Application.persistentDataPath, "user-data");
+            return Path.Combine(Directory.GetCurrentDirectory(), "user-data");
+        }
+    }
+    static string DataPath => Path.Combine(DataDirectory, "config.json");
+
+    public static PersistentData Instance {
+        get {
+            if (_instance == null) {
+                var fileData = "";
+                try {
+                    Debug.Log($"Application.persistentDataPath: {Application.persistentDataPath}, Current Directory: {Directory.GetCurrentDirectory()}");
+                    using var reader = new StreamReader(DataPath);
+                    fileData = reader.ReadToEnd();
+                    _instance = JsonConvert.DeserializeObject<PersistentData>(fileData);
+                } catch {
+                    _instance = new PersistentData();
+                }
+
+                if (JsonConvert.SerializeObject(_instance) != fileData) Save();
+            }
+            return _instance;
+        }
+    }
+
+
+    static bool _saving;
+    static bool _shouldResave;
+    public static void Save()
+    {
+        if (_saving) _shouldResave = true;
+        else _ = SaveInternal();
+    }
+
+    static async Task SaveInternal()
+    {
+        _saving = true;
+        await Task.Delay(10);
+        _shouldResave = false;
+
+        Debug.Log($"Writing PersistentData to {DataPath}");
+        Directory.CreateDirectory(DataDirectory);
+        {
+            using var writer = new StreamWriter(DataPath);
+            await writer.WriteAsync(JsonConvert.SerializeObject(_instance));
+        }
+        _saving = false;
+
+        if (_shouldResave) Save();
+    }
+
+    PersistentDataSaver() { }
+}
