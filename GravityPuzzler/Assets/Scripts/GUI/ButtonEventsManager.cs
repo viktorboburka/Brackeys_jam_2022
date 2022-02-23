29d31bdb83c994303ea2d411fb53aa269314a2c5
Isabella Skořepová <isabella@skorepova.info>
2022-02-23 16:51:06 +0100

    skip menu on first start

--- GravityPuzzler/Assets/Scripts/GUI/ButtonEventsManager.cs
+++ GravityPuzzler/Assets/Scripts/GUI/ButtonEventsManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonEventsManager : MonoBehaviour {
+    void Start()
+    {
+        if (PersistentData.Instance.lastLevel < 1) {
+            SceneManager.LoadScene(1);
+        }
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene(1);
