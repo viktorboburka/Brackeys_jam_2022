ce5f86b20b7ae557d355f2c4444cda1cf1e11a8a
Isabella Skořepová <isabella@skorepova.info>
2022-02-23 11:48:34 +0100

    deduplicate GravityControl

--- GravityPuzzler/Assets/Scripts/GravityControl.cs
+++ GravityPuzzler/Assets/Scripts/GravityControl.cs
@@ -2,21 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityControl : MonoBehaviour
-{
+public class GravityControl : MonoBehaviour {
+    public enum GravityDirection {
+        DOWN,
+        LEFT,
+        RIGHT,
+        UP,
+    }
+    [System.Serializable]
+    public class GravityChange {
+        public GravityDirection direction = GravityDirection.DOWN;
+        public float duration = 5;
+    }
+
+    [SerializeField]
+    private List<GravityChange> _timeline = new List<GravityChange>();
+
     [SerializeField]
     private float _terminalVelocity = 5.0f;
     [SerializeField]
     private float _gravityScale = 3.0f;
-    [SerializeField]
-    private List<string> gravChanges;
-    private int gravityChangesIdx = 0;
-    [SerializeField]
-    private List<float> gravChangeTimes;
-    private List<bool> gravChangesDone;
 
-    private Dictionary<string, Vector2> _gravityVectors;
-    private string _currentGravity = "down";
+    int timelineIndex = 0;
+    float timeSinceLast = 0;
+
+
+    private Dictionary<GravityDirection, Vector2> _gravityVectors;
 
     private CameraControl cameraControl;
     private Player player;
@@ -24,15 +35,9 @@ public class GravityControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gravChangesDone = new List<bool>();
-        for (int i = 0; i < gravChanges.Count; i++) {
-            gravChangesDone.Add(false);
-        }
         cameraControl = Camera.main.GetComponent<CameraControl>();//GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraControl>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-
         initGravity();
-        changeGravity("down");
     }
 
     // Update is called once per frame
@@ -41,49 +46,43 @@ public class GravityControl : MonoBehaviour
         triggerTimedGravityChanges();
     }
 
-    void triggerTimedGravityChanges() {
-        for (int i = 0; i < gravChanges.Count; i++) {
-            if (gravChangesDone[i]) {
-                continue;
-            }
-            float sumTime = 0.0f;
-            for(int j = 0; j <= i; j++) {
-                sumTime += gravChangeTimes[j];
-            }
-
-            if (Time.timeSinceLevelLoad > sumTime) {
-                changeGravity(gravChanges[i]);
-            }
+    void triggerTimedGravityChanges()
+    {
+        timeSinceLast += Time.deltaTime;
+        if (_timeline[timelineIndex].duration < timeSinceLast)
+        {
+            timeSinceLast -= _timeline[timelineIndex].duration;
+            timelineIndex = (timelineIndex + 1) % _timeline.Count;
+            changeGravity(_timeline[timelineIndex].direction);
         }
     }
 
 
-    public void changeGravity(string changeTo) {
-        Vector2 newGravity;
-        bool ret = _gravityVectors.TryGetValue(changeTo, out newGravity);
-        if (ret) {
+    private void changeGravity(GravityDirection changeTo)
+    {
+        if (_gravityVectors.TryGetValue(changeTo, out var newGravity))
+        {
             Physics2D.gravity = newGravity * _gravityScale;
-            _currentGravity = changeTo;
-            cameraControl.setTargetRotation(changeTo);
-            player.rotateControls(changeTo);
+            // TODO: change other scripts to use GravityDirection too
+            string stringified = changeTo.ToString().ToLower();
+            cameraControl.setTargetRotation(stringified);
+            player.rotateControls(stringified);
         }
     }
 
-    public string getCurrentGravity() {
-        return _currentGravity;
-    }
-
-    void initGravity() {
+    void initGravity()
+    {
         Physics2D.gravity *= _gravityScale;
 
-        _gravityVectors = new Dictionary<string, Vector2>();
-        _gravityVectors.Add("up", new Vector2(0.0f, 9.8f));
-        _gravityVectors.Add("down", new Vector2(0.0f, -9.8f));
-        _gravityVectors.Add("left", new Vector2(-9.8f, 0.0f));
-        _gravityVectors.Add("right", new Vector2(9.8f, 0.0f));
+        _gravityVectors = new Dictionary<GravityDirection, Vector2>();
+        _gravityVectors.Add(GravityDirection.UP, new Vector2(0.0f, 9.8f));
+        _gravityVectors.Add(GravityDirection.DOWN, new Vector2(0.0f, -9.8f));
+        _gravityVectors.Add(GravityDirection.LEFT, new Vector2(-9.8f, 0.0f));
+        _gravityVectors.Add(GravityDirection.RIGHT, new Vector2(9.8f, 0.0f));
 
         GameObject[] allRigidBodyObjects = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
-        foreach (GameObject go in allRigidBodyObjects) {
+        foreach (GameObject go in allRigidBodyObjects)
+        {
             //Debug.Log(go.ToString());
             Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
             if (rb == null) continue;
@@ -91,10 +90,12 @@ public class GravityControl : MonoBehaviour
         }
     }
 
-    public static float GetDrag(float aVelocityChange, float aFinalVelocity) {
+    public static float GetDrag(float aVelocityChange, float aFinalVelocity)
+    {
         return aVelocityChange / ((aFinalVelocity + aVelocityChange) * Time.fixedDeltaTime);
     }
-    public static float GetDragFromAcceleration(float aAcceleration, float aFinalVelocity) {
+    public static float GetDragFromAcceleration(float aAcceleration, float aFinalVelocity)
+    {
         return GetDrag(aAcceleration * Time.fixedDeltaTime, aFinalVelocity);
     }
 }
