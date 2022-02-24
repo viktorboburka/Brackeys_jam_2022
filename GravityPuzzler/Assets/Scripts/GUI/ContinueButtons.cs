27ecf47c8edb971b095b0d979e51802351d549fd
Isabella Skořepová <isabella@skorepova.info>
2022-02-24 19:26:45 +0100

    layout main menu better

--- GravityPuzzler/Assets/Scripts/GUI/ContinueButtons.cs
+++ GravityPuzzler/Assets/Scripts/GUI/ContinueButtons.cs
@@ -9,13 +9,9 @@ public class ContinueButtons : MonoBehaviour {
     {
         var lastLevel = PersistentData.Instance.lastLevel;
         var prefab = Resources.Load<GameObject>("Load Level");
-        const int gap = 40;
-        const int width = 100;
-        var x = -(width + gap) * (lastLevel - 1) / 2;
+
         for (var i = 0; i < lastLevel; ++i) {
             var obj = Instantiate(prefab, transform);
-            obj.transform.localPosition = new Vector3(x, 0, 0);
-            x += width + gap;
             var text = obj.transform.GetComponentInChildren<Text>();
             text.text = $"Level {i + 1}";
             var loader = obj.GetComponent<LoadScene>();
