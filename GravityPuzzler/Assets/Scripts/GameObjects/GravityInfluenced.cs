0c1641ef874d752ade1f30a2d953775d3361959b
viktorboburka <viktor@boburka.cz>
2022-02-24 17:16:34 +0100

    level 3, levels 1 and 2 switched, adjusted reachable positions in gravityinfluenced objects

--- GravityPuzzler/Assets/Scripts/GameObjects/GravityInfluenced.cs
+++ GravityPuzzler/Assets/Scripts/GameObjects/GravityInfluenced.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GravityInfluenced : MonoBehaviour
 {
+
+    private int _reachableWidth = 50;
+    private int _reachableHeight = 50;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +21,21 @@ public class GravityInfluenced : MonoBehaviour
     }
 
     private void MoveThroughEdges() {
-        if (gameObject.transform.position.y < -50) {
-            Vector2 newPos = new Vector2(gameObject.transform.position.x, 50);
+        if (gameObject.transform.position.y < -_reachableHeight) {
+            Vector2 newPos = new Vector2(gameObject.transform.position.x, _reachableHeight);
             gameObject.transform.position = newPos;
         }
-        if (gameObject.transform.position.y > 50) {
-            Vector2 newPos = new Vector2(gameObject.transform.position.x, -50);
+        if (gameObject.transform.position.y > _reachableHeight) {
+            Vector2 newPos = new Vector2(gameObject.transform.position.x, -_reachableHeight);
             gameObject.transform.position = newPos;
         }
 
-        if (gameObject.transform.position.x < -90) {
-            Vector2 newPos = new Vector2(90, gameObject.transform.position.y);
+        if (gameObject.transform.position.x < -_reachableWidth) {
+            Vector2 newPos = new Vector2(_reachableWidth, gameObject.transform.position.y);
             gameObject.transform.position = newPos;
         }
-        if (gameObject.transform.position.x > 90) {
-            Vector2 newPos = new Vector2(-90, gameObject.transform.position.y);
+        if (gameObject.transform.position.x > _reachableWidth) {
+            Vector2 newPos = new Vector2(-_reachableWidth, gameObject.transform.position.y);
             gameObject.transform.position = newPos;
         }
     }
